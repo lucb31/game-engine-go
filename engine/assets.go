15d7c4337e55410867563e35fa24cba640d38436
Lucas Berchtold <Lucas.Berchtold@audimex.com>
2024-07-28 06:09:14 +0200

    refactor - Move asset management to separate struct

--- engine/assets.go
+++ engine/assets.go
@@ -1,5 +1,53 @@
 package engine
 
+import (
+	"bytes"
+	"fmt"
+	"image"
+	"os"
+
+	"github.com/hajimehoshi/ebiten/v2"
+)
+
+type AssetManager struct {
+	PlainsTileset *ebiten.Image
+	PlayerTileset *ebiten.Image
+	TilesPerRow   int
+	TileSize      int
+}
+
+func NewAssetManager() (*AssetManager, error) {
+	// Load assets
+	var err error
+	im, err := ReadPngAsset("assets/player.png")
+	if err != nil {
+		fmt.Println("Could not read assets!", err.Error())
+		return nil, err
+	}
+	playerImage := ebiten.NewImageFromImage(im)
+	im, err = ReadPngAsset("assets/plains.png")
+	if err != nil {
+		fmt.Println("Could not read plains asset!", err.Error())
+		return nil, err
+	}
+	plainsTileset := ebiten.NewImageFromImage(im)
+	tileSize := 16
+	tilesPerRow := int(plainsTileset.Bounds().Dx() / tileSize)
+	return &AssetManager{PlayerTileset: playerImage, PlainsTileset: plainsTileset, TilesPerRow: tilesPerRow, TileSize: tileSize}, nil
+}
+
+func ReadPngAsset(path string) (image.Image, error) {
+	dat, err := os.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+	im, _, err := image.Decode(bytes.NewReader(dat))
+	if err != nil {
+		return nil, err
+	}
+	return im, nil
+}
+
 type GameObj struct {
 	asset           *GameAsset
 	activeAnimation string
--- main.go
+++ main.go
@@ -1,11 +1,9 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
 	"image"
 	_ "image/png"
-	"os"
 
 	"github.com/lucb31/animation-go/engine"
 
@@ -18,24 +16,25 @@ const (
 	frameWidth          = 48
 	frameHeight         = 48
 	animationFrameCount = 6
+	tileSize            = 16
 )
 
-// Assets
-
-var plainsTileset *ebiten.Image
-var playerImage *ebiten.Image
-
 type Game struct {
-	frameCount int64
-	world      engine.GameWorld
+	frameCount   int64
+	world        engine.GameWorld
+	assetManager *engine.AssetManager
 }
 
 func Init() (*Game, error) {
+	am, err := engine.NewAssetManager()
+	if err != nil {
+		return nil, err
+	}
 	world, err := engine.NewWorld(12, 8)
 	if err != nil {
 		return nil, err
 	}
-	return &Game{world: world}, nil
+	return &Game{world: world, assetManager: am}, nil
 }
 
 func (g *Game) Update() error {
@@ -48,25 +47,13 @@ func (g *Game) Update() error {
 	return nil
 }
 
-func keyPressed(haystack []ebiten.Key, needle string) bool {
-	for _, val := range haystack {
-		if val.String() == needle {
-			return true
-		}
-	}
-	return false
-}
-
 func (g *Game) Draw(screen *ebiten.Image) {
 	g.drawBiomes(screen)
 	g.drawPlayer(screen)
 }
 
-const tileSize = 16
-
 func (g *Game) drawBiomes(screen *ebiten.Image) {
 	// Todo this doesnt change per frame
-	tilesPerRow := int(plainsTileset.Bounds().Dx() / tileSize)
 	// Todo Currently drawing WHOLE map. Should only draw visible map
 	for row := range g.world.Height {
 		for col := range g.world.Width {
@@ -76,20 +63,14 @@ func (g *Game) drawBiomes(screen *ebiten.Image) {
 
 			// Select correct tile from tileset
 			tileIdx := int(g.world.Biome[row][col])
-			tileX := tileIdx % tilesPerRow
-			tileY := int(tileIdx / tilesPerRow)
+			tileX := tileIdx % g.assetManager.TilesPerRow
+			tileY := int(tileIdx / g.assetManager.TilesPerRow)
 
-			// 0 => 0, 0
-			// 2 => 32, 0
-			// 4 => 64, 0
-			// 6 => 96, 0
-			// 7 => 0, 16
-			subIm := plainsTileset.SubImage(image.Rect(tileSize*tileX, tileSize*tileY, tileSize*(tileX+1), tileSize*(tileY+1))).(*ebiten.Image)
+			subIm := g.assetManager.PlainsTileset.SubImage(image.Rect(tileSize*tileX, tileSize*tileY, tileSize*(tileX+1), tileSize*(tileY+1))).(*ebiten.Image)
 			screen.DrawImage(subIm, &op)
 
 		}
 	}
-	// g.world.Biom
 }
 
 func (g *Game) drawPlayer(screen *ebiten.Image) {
@@ -101,7 +82,7 @@ func (g *Game) drawPlayer(screen *ebiten.Image) {
 
 	animationFrame := int(g.frameCount/6) % animationFrameCount
 	tilePosition := 4
-	subIm := playerImage.SubImage(image.Rect(
+	subIm := g.assetManager.PlayerTileset.SubImage(image.Rect(
 		animationFrame*frameWidth,
 		tilePosition*frameHeight,
 		(animationFrame+1)*frameWidth,
@@ -114,31 +95,7 @@ func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
 	return screenWidth, screenHeight
 }
 
-func ReadPngAsset(path string) (image.Image, error) {
-	dat, err := os.ReadFile(path)
-	if err != nil {
-		return nil, err
-	}
-	im, _, err := image.Decode(bytes.NewReader(dat))
-	if err != nil {
-		return nil, err
-	}
-	return im, nil
-}
-
 func main() {
-	// Load assets
-	var err error
-	im, err := ReadPngAsset("assets/player.png")
-	if err != nil {
-		fmt.Println("Could not read assets!", err.Error())
-	}
-	playerImage = ebiten.NewImageFromImage(im)
-	im, err = ReadPngAsset("assets/plains.png")
-	if err != nil {
-		fmt.Println("Could not read plains asset!", err.Error())
-	}
-	plainsTileset = ebiten.NewImageFromImage(im)
 	ebiten.SetWindowSize(1024, 768)
 	ebiten.SetWindowTitle("Animate")
 
