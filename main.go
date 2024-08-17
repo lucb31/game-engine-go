e35d53ec1402d80b0b9ae74b272979720d2963b2
Lucas Berchtold <Lucas.Berchtold@audimex.com>
2024-08-17 11:49:15 +0200

    feat: Add cli flag to choose which game to start

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 
+	"github.com/lucb31/game-engine-go/survival"
 	"github.com/lucb31/game-engine-go/td"
 
 	"github.com/hajimehoshi/ebiten/v2"
@@ -15,10 +17,24 @@ const (
 
 func main() {
 	ebiten.SetWindowSize(1024, 768)
-	ebiten.SetWindowTitle("Animate")
+	ebiten.SetWindowTitle("Game engine")
+
+	// CLI
+	var gameSelected string
+	flag.StringVar(&gameSelected, "g", "survival", "Option to select game. Currently available 'td' & 'survival'")
+	flag.Parse()
 
 	// Init game
-	g, err := td.NewTDGame(screenWidth, screenHeight)
+	var g ebiten.Game
+	var err error
+	switch gameSelected {
+	case "td":
+		g, err = td.NewTDGame(screenWidth, screenHeight)
+	case "survival":
+		g, err = survival.NewSurvivalGame(screenWidth, screenHeight)
+	default:
+		panic("No game found")
+	}
 	if err != nil {
 		panic(err)
 	}
